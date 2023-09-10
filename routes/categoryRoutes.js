8bc7f8a6e223691865bce5438d9712c842dd1bed
Ahmadok99 <ahmadoxk@gmail.com>
2023-09-10 09:40:19 +0300

    updated

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,9 +4,9 @@ const auth = require('../middleware/auth');
 const { create, edit, delete: destroy, list, get } = require('../controllers/categoryController');
 const validateCategoryData = require('../vaildators/validateCategoryData')
 
-router.post('/create'  ,auth, create);
+router.post('/create'  ,auth,validateCategoryData, create);
 router.put('/edit/:id',auth ,validateCategoryData, edit);
-router.delete('/delete/:id',auth ,validateCategoryData, destroy);
+router.delete('/delete/:id',auth , destroy);
 router.get('/get/:id',auth, get);
 router.get('/list',auth, list);
 
